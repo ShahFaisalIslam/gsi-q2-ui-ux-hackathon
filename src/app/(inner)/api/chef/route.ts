eb968fee59c440b68fe8a2235d91997866914e47
Shah Faisal <faisal.islam.ceme@gmail.com>
2025-01-18 23:18:17 +0500

    small stuff to get deployment underway

--- src/app/(inner)/api/chef/route.ts
+++ src/app/(inner)/api/chef/route.ts
@@ -2,23 +2,12 @@ import { GetChefCardData, GetChefCardDataAll } from "@/sanity/sanity.query";
 
 
 export async function GET(Request: Request) {
-    const {searchParams} = new URL(Request.url);
+    let data: any;
+    const { searchParams } = new URL(Request.url);
     const id = searchParams.get("id");
-    if (id === null) {
-        const data = await GetChefCardDataAll();      
-        return Response.json(data);
-    }
-    const data = await GetChefCardData(id);
+    if (id === null)
+        data = await GetChefCardDataAll();
+    else
+        data = await GetChefCardData(id);
     return Response.json(data);
 }
-
-// export async function GET(Request: Request) {
-//     const {searchParams} = new URL(Request.url);
-//     const id = searchParams.get("id");
-//     if (id === null) {
-//         const data = await GetFoodCardDataAll();
-//         return Response.json(data);    
-//     }
-//     const data = await GetFoodCardData(id);
-//     return Response.json(data);
-// }
--- src/app/(inner)/api/food/route.ts
+++ src/app/(inner)/api/food/route.ts
@@ -2,12 +2,13 @@ import { GetFoodCardData, GetFoodCardDataAll } from "@/sanity/sanity.query";
 
 
 export async function GET(Request: Request) {
-    const {searchParams} = new URL(Request.url);
+    let data: any;
+    const { searchParams } = new URL(Request.url);
     const id = searchParams.get("id");
-    if (id === null) {
-        const data = await GetFoodCardDataAll();
-        return Response.json(data);    
-    }
-    const data = await GetFoodCardData(id);
+    if (id === null)
+        data = await GetFoodCardDataAll();
+    else
+        data = await GetFoodCardData(id);
+
     return Response.json(data);
 }
